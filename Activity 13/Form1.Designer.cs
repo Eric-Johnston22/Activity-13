2be89537c93b9da2e3115dfbedc30fe651c7eae7
Eric-Johnston <eric.mjohnston@protonmail.com>
2022-08-26 23:44:09 -0400

    Add project files.

---
+++ Activity 13/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Activity_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbx_one = new System.Windows.Forms.TextBox();
+            this.tbx_two = new System.Windows.Forms.TextBox();
+            this.tbx_three = new System.Windows.Forms.TextBox();
+            this.tbx_four = new System.Windows.Forms.TextBox();
+            this.tbx_five = new System.Windows.Forms.TextBox();
+            this.tbx_six = new System.Windows.Forms.TextBox();
+            this.tbx_seven = new System.Windows.Forms.TextBox();
+            this.tbx_eight = new System.Windows.Forms.TextBox();
+            this.tbx_nine = new System.Windows.Forms.TextBox();
+            this.btn_newGame = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lbl_wins = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbx_one
+            // 
+            this.tbx_one.Location = new System.Drawing.Point(246, 65);
+            this.tbx_one.Name = "tbx_one";
+            this.tbx_one.Size = new System.Drawing.Size(75, 75);
+            this.tbx_one.TabIndex = 0;
+            // 
+            // tbx_two
+            // 
+            this.tbx_two.Location = new System.Drawing.Point(330, 65);
+            this.tbx_two.Name = "tbx_two";
+            this.tbx_two.Size = new System.Drawing.Size(75, 75);
+            this.tbx_two.TabIndex = 1;
+            // 
+            // tbx_three
+            // 
+            this.tbx_three.Location = new System.Drawing.Point(414, 65);
+            this.tbx_three.Name = "tbx_three";
+            this.tbx_three.Size = new System.Drawing.Size(75, 75);
+            this.tbx_three.TabIndex = 2;
+            // 
+            // tbx_four
+            // 
+            this.tbx_four.Location = new System.Drawing.Point(246, 146);
+            this.tbx_four.Name = "tbx_four";
+            this.tbx_four.Size = new System.Drawing.Size(75, 75);
+            this.tbx_four.TabIndex = 3;
+            // 
+            // tbx_five
+            // 
+            this.tbx_five.Location = new System.Drawing.Point(330, 146);
+            this.tbx_five.Name = "tbx_five";
+            this.tbx_five.Size = new System.Drawing.Size(75, 75);
+            this.tbx_five.TabIndex = 4;
+            // 
+            // tbx_six
+            // 
+            this.tbx_six.Location = new System.Drawing.Point(414, 146);
+            this.tbx_six.Name = "tbx_six";
+            this.tbx_six.Size = new System.Drawing.Size(75, 75);
+            this.tbx_six.TabIndex = 5;
+            // 
+            // tbx_seven
+            // 
+            this.tbx_seven.Location = new System.Drawing.Point(246, 227);
+            this.tbx_seven.Name = "tbx_seven";
+            this.tbx_seven.Size = new System.Drawing.Size(75, 75);
+            this.tbx_seven.TabIndex = 6;
+            // 
+            // tbx_eight
+            // 
+            this.tbx_eight.Location = new System.Drawing.Point(330, 227);
+            this.tbx_eight.Name = "tbx_eight";
+            this.tbx_eight.Size = new System.Drawing.Size(75, 75);
+            this.tbx_eight.TabIndex = 7;
+            // 
+            // tbx_nine
+            // 
+            this.tbx_nine.Location = new System.Drawing.Point(414, 227);
+            this.tbx_nine.Name = "tbx_nine";
+            this.tbx_nine.Size = new System.Drawing.Size(75, 75);
+            this.tbx_nine.TabIndex = 8;
+            // 
+            // btn_newGame
+            // 
+            this.btn_newGame.Location = new System.Drawing.Point(290, 350);
+            this.btn_newGame.Name = "btn_newGame";
+            this.btn_newGame.Size = new System.Drawing.Size(75, 23);
+            this.btn_newGame.TabIndex = 9;
+            this.btn_newGame.Text = "button1";
+            this.btn_newGame.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(371, 350);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // lbl_wins
+            // 
+            this.lbl_wins.AutoSize = true;
+            this.lbl_wins.Location = new System.Drawing.Point(348, 318);
+            this.lbl_wins.Name = "lbl_wins";
+            this.lbl_wins.Size = new System.Drawing.Size(35, 13);
+            this.lbl_wins.TabIndex = 11;
+            this.lbl_wins.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_wins);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_newGame);
+            this.Controls.Add(this.tbx_nine);
+            this.Controls.Add(this.tbx_eight);
+            this.Controls.Add(this.tbx_seven);
+            this.Controls.Add(this.tbx_six);
+            this.Controls.Add(this.tbx_five);
+            this.Controls.Add(this.tbx_four);
+            this.Controls.Add(this.tbx_three);
+            this.Controls.Add(this.tbx_two);
+            this.Controls.Add(this.tbx_one);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbx_one;
+        private System.Windows.Forms.TextBox tbx_two;
+        private System.Windows.Forms.TextBox tbx_three;
+        private System.Windows.Forms.TextBox tbx_four;
+        private System.Windows.Forms.TextBox tbx_five;
+        private System.Windows.Forms.TextBox tbx_six;
+        private System.Windows.Forms.TextBox tbx_seven;
+        private System.Windows.Forms.TextBox tbx_eight;
+        private System.Windows.Forms.TextBox tbx_nine;
+        private System.Windows.Forms.Button btn_newGame;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lbl_wins;
+    }
+}
+
